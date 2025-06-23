4305e06de9cccfbeef86b2ab817b06611bc2b1ac
Bolin Huang <ibh@live.com>
2025-06-22 23:15:11 -0700

    update keymap for macOS dvorak

--- keyboards/wilba_tech/wt65_h2/keymaps/dvorak/keymap.c
+++ keyboards/wilba_tech/wt65_h2/keymaps/dvorak/keymap.c
@@ -16,22 +16,26 @@
 
 // Dvorak layout for WT65-H2
 #include QMK_KEYBOARD_H
+#include <stdbool.h>
 
 enum custom_layers {
-    _DVORAK,
+    _WIN,
+    _MACOS,
     _QWERTY,
     _FN
 };
 
 enum custom_keycodes {
     CTL_QWERTY = SAFE_RANGE,
-    TOGGLE_LAYOUT
+    GUI_QWERTY,
+    PREV_LAYOUT,
+    NEXT_LAYOUT
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     // Dvorak layer
-    [_DVORAK] = LAYOUT_all(
+    [_WIN] = LAYOUT_all(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_LBRC,  KC_RBRC,  KC_BSPC, KC_GRV,  KC_HOME,
         KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH,  KC_EQL,   KC_NUBS,          KC_END,
         MO(_FN), KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS,  KC_ENT,                     KC_PGUP,
@@ -41,18 +45,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     // QWERTY Layer
     [_QWERTY] = LAYOUT_all(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC, KC_GRV,  KC_HOME,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC, KC_GRV,  KC_HOME,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_NUBS,          KC_END,
         MO(_FN), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_ENT,                    KC_PGUP,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                    KC_UP,   KC_PGDN,
-        KC_LCTL,          KC_LALT,                            KC_SPC,                                      KC_RGUI,           KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL,          KC_LGUI,                            KC_SPC,                                      KC_RGUI,           KC_LEFT, KC_DOWN, KC_RGHT
+    ),
+
+    // macOS layer
+    [_MACOS] = LAYOUT_all(
+        QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_LBRC,  KC_RBRC,  KC_BSPC, KC_GRV,  KC_HOME,
+        KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH,  KC_EQL,   KC_NUBS,          KC_END,
+        MO(_FN), KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS,  KC_ENT,                     KC_PGUP,
+        KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT,                     KC_UP,   KC_PGDN,
+        KC_LCTL,          GUI_QWERTY,                         KC_SPC,                                      KC_RALT,            KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     [_FN] = LAYOUT_all(
         KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,  KC_NO,   KC_NO,  KC_NO,
         KC_CAPS, KC_MPLY, KC_MPRV, KC_MNXT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_UP,   KC_NO,   KC_NO,    KC_NO,    KC_NO,   KC_NO,           QK_BOOT,
-        KC_NO,   KC_MUTE, KC_VOLD, KC_VOLU, KC_NO,   KC_NO,   KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,    KC_DEL,   KC_NO,                    KC_NO,
-        KC_LSFT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,                      KC_NO,  TOGGLE_LAYOUT,
+        KC_NO,   KC_MUTE, KC_VOLD, KC_VOLU, KC_NO,   KC_NO,   KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,    KC_DEL,   KC_NO,                    PREV_LAYOUT,
+        KC_LSFT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,                      KC_NO,  NEXT_LAYOUT,
         KC_NO,            KC_NO,                              KC_NO,                                        KC_NO,            KC_NO,    KC_NO,  KC_NO
     ),
 };
@@ -76,14 +89,45 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             // Skip all further processing of this key
             return false;
 
-        case TOGGLE_LAYOUT:
+        case GUI_QWERTY:
+            if (record->event.pressed) {
+                // Turn on QWERTY when Command is pressed
+                layer_on(_QWERTY);
+                // Register Command as a mod key
+                register_mods(MOD_BIT(KC_LGUI)); 
+            } else {
+                // Turn off QWERTY when Command is released
+                layer_off(_QWERTY); 
+                // Unregister Command mod key
+                unregister_mods(MOD_BIT(KC_LGUI)); 
+            }
+            // Skip all further processing of this key
+            return false;
+
+        case PREV_LAYOUT:
             if (record->event.pressed) {
                 // Get the current default layer state
                 uint8_t default_layer = get_highest_layer(default_layer_state);
-                if (default_layer == _DVORAK) {
-                    set_single_persistent_default_layer(_QWERTY);
+                if (default_layer == _WIN) {
+                    set_single_persistent_default_layer(_MACOS);
                 } else if (default_layer == _QWERTY){
-                    set_single_persistent_default_layer(_DVORAK);
+                    set_single_persistent_default_layer(_WIN);
+                } else if (default_layer == _MACOS) {
+                    set_single_persistent_default_layer(_QWERTY);
+                }
+            }
+            return false;
+
+        case NEXT_LAYOUT:
+            if (record->event.pressed) {
+                // Get the current default layer state
+                uint8_t default_layer = get_highest_layer(default_layer_state);
+                if (default_layer == _WIN) {
+                    set_single_persistent_default_layer(_QWERTY);
+                } else if (default_layer == _QWERTY) {
+                    set_single_persistent_default_layer(_MACOS);
+                } else if (default_layer == _MACOS) {
+                    set_single_persistent_default_layer(_WIN);
                 }
             }
             return false;
